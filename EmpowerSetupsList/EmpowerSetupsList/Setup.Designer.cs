00015f641f686fab05553a15cb823ff08a20a5c3
satish rajana <satishrajana@gmail.com>
2017-10-17 21:26:25 -0400

    ListSetups
    Folder

---
+++ EmpowerSetupsList/EmpowerSetupsList/Setup.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace EmpowerSetupsList
+{
+    partial class Setup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFolder = new System.Windows.Forms.TextBox();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnGetQuickCode = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnExportToExcel = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.cmbSelect = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnClearSrch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Plugin Folder";
+            // 
+            // txtFolder
+            // 
+            this.txtFolder.Location = new System.Drawing.Point(121, 28);
+            this.txtFolder.Name = "txtFolder";
+            this.txtFolder.Size = new System.Drawing.Size(646, 20);
+            this.txtFolder.TabIndex = 1;
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnSelectFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelectFolder.Location = new System.Drawing.Point(785, 28);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(119, 23);
+            this.btnSelectFolder.TabIndex = 2;
+            this.btnSelectFolder.Text = "Select Folder";
+            this.btnSelectFolder.UseVisualStyleBackColor = false;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // btnGetQuickCode
+            // 
+            this.btnGetQuickCode.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnGetQuickCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGetQuickCode.Location = new System.Drawing.Point(31, 615);
+            this.btnGetQuickCode.Name = "btnGetQuickCode";
+            this.btnGetQuickCode.Size = new System.Drawing.Size(210, 45);
+            this.btnGetQuickCode.TabIndex = 4;
+            this.btnGetQuickCode.Text = "Get Setup Description";
+            this.btnGetQuickCode.UseVisualStyleBackColor = true;
+            this.btnGetQuickCode.Click += new System.EventHandler(this.btnQuickCode_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(31, 680);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(554, 18);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Tag = "";
+            this.lblStatus.Text = "Folder Ex : C:\\Empower\\BASE_7_0_1_SP1\\Empower.NET\\Setups\\Empower.Setups.Plugins";
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Transparent;
+            this.btnExit.FlatAppearance.BorderSize = 4;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(835, 666);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(113, 32);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(31, 135);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView1.Size = new System.Drawing.Size(917, 474);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // btnExportToExcel
+            // 
+            this.btnExportToExcel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnExportToExcel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExportToExcel.Location = new System.Drawing.Point(835, 615);
+            this.btnExportToExcel.Name = "btnExportToExcel";
+            this.btnExportToExcel.Size = new System.Drawing.Size(113, 28);
+            this.btnExportToExcel.TabIndex = 8;
+            this.btnExportToExcel.Text = "Export to Excel";
+            this.btnExportToExcel.UseVisualStyleBackColor = true;
+            this.btnExportToExcel.Click += new System.EventHandler(this.btnExportToExcel_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(816, 86);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(95, 26);
+            this.btnSearch.TabIndex = 9;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(549, 91);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(261, 20);
+            this.txtSearch.TabIndex = 10;
+            // 
+            // cmbSelect
+            // 
+            this.cmbSelect.CausesValidation = false;
+            this.cmbSelect.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbSelect.FormattingEnabled = true;
+            this.cmbSelect.Items.AddRange(new object[] {
+            "QuickCode",
+            "Description",
+            "MasterTable",
+            "ChildTable"});
+            this.cmbSelect.Location = new System.Drawing.Point(333, 92);
+            this.cmbSelect.Name = "cmbSelect";
+            this.cmbSelect.Size = new System.Drawing.Size(169, 21);
+            this.cmbSelect.TabIndex = 11;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnSelectFolder);
+            this.groupBox1.Controls.Add(this.btnSearch);
+            this.groupBox1.Controls.Add(this.cmbSelect);
+            this.groupBox1.Controls.Add(this.txtSearch);
+            this.groupBox1.Controls.Add(this.txtFolder);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(31, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(917, 117);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(202, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 18);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Search by Column :";
+            // 
+            // btnClearSrch
+            // 
+            this.btnClearSrch.Location = new System.Drawing.Point(580, 615);
+            this.btnClearSrch.Name = "btnClearSrch";
+            this.btnClearSrch.Size = new System.Drawing.Size(127, 23);
+            this.btnClearSrch.TabIndex = 13;
+            this.btnClearSrch.Text = "Clear Search";
+            this.btnClearSrch.UseVisualStyleBackColor = true;
+            this.btnClearSrch.Click += new System.EventHandler(this.btnClearSrch_Click);
+            // 
+            // Setup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(983, 732);
+            this.Controls.Add(this.btnClearSrch);
+            this.Controls.Add(this.btnExportToExcel);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnGetQuickCode);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Setup";
+            this.Text = "Get Setup Description";
+            this.Load += new System.EventHandler(this.Setup_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFolder;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnGetQuickCode;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnExportToExcel;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.ComboBox cmbSelect;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnClearSrch;
+    }
+}
---
+++ EmpowerSetupsList/EmpowerSetupsList/Setup.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace EmpowerSetupsList
+{
+    public partial class Setup : Form
+    {
+        public Setup()
+        {
+            InitializeComponent();
+            btnSearch.Enabled = false;
+            btnExportToExcel.Enabled = false;
+            txtSearch.Text = string.Empty;
+            btnClearSrch.Enabled = false;
+        }
+
+        private void btnQuickCode_Click(object sender, EventArgs e)
+        {
+            if (txtFolder.Text == string.Empty)
+            {
+                lblStatus.Text = "Enter a valid plug folder path : /Empower.NET/Setups/Empower.Setups.Plugins";
+                return;
+            };
+
+            SeedTheGrid();
+           
+        }
+
+        public void SeedTheGrid()
+        {
+            DataTable QcTable = new DataTable();
+            DataTable dt = new DataTable();
+            txtSearch.Text = string.Empty;
+
+
+            try
+            {
+                //Get List of Quickcode and Description
+                DataTable Codes = GetQuickCodes(QcTable);
+                //Get List of Description, Master and Child Table
+                DataTable MCTables = GetMasterChildTables(dt);
+
+                //Clone table A to add columns from A to target tabe
+                DataTable targetTable = QcTable.Clone();
+                //select columns from B that need to be added to target table
+                var dt2Columns = dt.Columns.OfType<DataColumn>().Select(dc =>
+                new DataColumn(dc.ColumnName, dc.DataType, dc.Expression, dc.ColumnMapping));
+                //remove from target table, column that is common between A and B
+                var dt2FinalColumns = from dc in dt2Columns.AsEnumerable()
+                                      where targetTable.Columns.Contains(dc.ColumnName) == false
+                                      select dc;
+                //Add columns from B to target table					  
+                targetTable.Columns.AddRange(dt2FinalColumns.ToArray());
+                //retrieve rows from A and B - minus the Description column that is common
+                var rowData = from row1 in QcTable.AsEnumerable()
+                              join row2 in dt.AsEnumerable()
+                              on row1.Field<string>("Description") equals row2.Field<string>("Description")
+                              orderby row1.Field<string>("Description")
+                              select row1.ItemArray.Concat(row2.ItemArray.Where(r2 => row1.ItemArray.Contains(r2) == false)).ToArray();
+                //add rows to target table			  
+                foreach (object[] values in rowData)
+                    targetTable.Rows.Add(values);
+                targetTable.DefaultView.Sort = "QuickCode ASC";
+
+                dataGridView1.DataSource = targetTable;
+                dataGridView1.AutoResizeColumns();
+                dataGridView1.ScrollBars = ScrollBars.Vertical;
+
+                foreach (DataGridViewBand band in dataGridView1.Columns)
+                {
+                    band.ReadOnly = true;
+                }
+                lblStatus.Text = "Total Number of Rows :" + (dataGridView1.RowCount - 1).ToString();
+
+                //textBox1.Text = @"C:\Empower\BASE_7_0_1_SP1\Empower.NET\Setups\Empower.Setups.Plugins";
+                btnExportToExcel.Enabled = true;
+                btnSearch.Enabled = true;
+                
+
+            }
+            catch (DirectoryNotFoundException DirNotFound)
+            {
+                lblStatus.Text = DirNotFound.Message;
+            }
+            catch (UnauthorizedAccessException UnAuthDir)
+            {
+                lblStatus.Text = UnAuthDir.Message;
+            }
+            catch (PathTooLongException LongPath)
+            {
+                lblStatus.Text = LongPath.Message;
+            }
+            catch (System.ArgumentException ane)
+            {
+                lblStatus.Text = "Select a valid folder";
+                lblStatus.ForeColor = System.Drawing.Color.Red;
+            }
+        }
+        public DataTable GetQuickCodes(DataTable QcTable)
+        {
+
+            QcTable.Clear();
+            QcTable.Columns.Add("QuickCode", typeof(string));
+            QcTable.Columns.Add("Description", typeof(string));
+
+            string foldername = txtFolder.Text;
+            var files = from file in Directory.EnumerateFiles(foldername, "*.cs", SearchOption.AllDirectories)
+                        from line in File.ReadLines(file)
+                        where line.Contains("Plugin(")
+                        select new
+                        { File = file, Line = line };
+
+
+            foreach (var f in files)
+            {
+                String l = f.Line;
+                string fl = Path.GetFileName(f.File).Split('.')[0];
+                int startidx = l.IndexOf("Plugin(\"", 0, StringComparison.CurrentCulture);
+
+                if (startidx > 0)
+                {
+                    int endidx = l.Length;
+                    string result = l.Substring(startidx, endidx - startidx);
+                    Regex regex = new Regex(".*?\\(.*?,.*?, .*?, \"(?<WhatEverPP01Is>.*?)\",.*");
+
+                    var match = regex.Match(result);
+
+                    if (match.Success)
+                    {
+                        var groupValue = fl;
+                        var whateverPP01Is = match.Groups["WhatEverPP01Is"].Value;
+                        String str = whateverPP01Is + " - " + groupValue;
+                        QcTable.Rows.Add(whateverPP01Is, groupValue);
+                    }
+                }
+            }
+            return QcTable;
+        }
+
+        public DataTable GetMasterChildTables(DataTable dt)
+
+        {
+            string foldername = txtFolder.Text;
+            var files = from file in Directory.EnumerateFiles(foldername, "*.designer.cs", SearchOption.AllDirectories)
+                        from line in File.ReadLines(file)
+                        where line.Contains("TableName")
+                        orderby Path.GetFileName(file).Split('.')[0]
+                        select new
+                        { File = file, Line = line };
+
+            string cn, pcn, ftn, tn, ptn, pctn;
+            cn = pcn = ftn = tn = ptn = pctn = string.Empty;
+
+            dt.Clear();
+            dt.Columns.Add("Description");
+            dt.Columns.Add("MasterTable");
+            dt.Columns.Add("ChildTable");
+
+            foreach (var f in files)
+            {
+                //Displays the line that has the TABLENAME
+                String l = f.Line;
+                //Displays the file name
+                string fl = f.File;
+                cn = Path.GetFileName(fl).Split('.')[0];
+
+                Regex reg = new Regex("\"(?<Group>.*?)\"");
+                var match = reg.Match(l);
+                tn = match.Groups["Group"].Value;
+
+                if (pcn == "" && ptn == "" && pctn == "")
+                {
+                    pcn = cn; ptn = tn;
+                }
+                if (cn == pcn && ptn != "" && pcn != "")
+                {
+                    pctn = tn;
+                }
+                if (cn != pcn && pcn != "")
+                {
+                    if (ptn != "" || pctn != "")
+                    {
+                        ftn = ptn + "			" + pctn;
+                        Console.WriteLine(pcn + "				" + ftn);
+                        dt.Rows.Add(pcn, ptn, pctn);
+                    }
+                    pcn = cn; ptn = tn; pctn = "";
+                }
+            }
+            return dt;
+        }
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = "";
+            FolderBrowserDialog fb = new FolderBrowserDialog();
+            fb.ShowNewFolderButton = false;
+            fb.SelectedPath = @"C:";
+            
+            if (fb.ShowDialog() == DialogResult.OK)
+            {
+                txtFolder.Text = fb.SelectedPath;
+            }
+            lblStatus.Text = string.Empty;
+        }
+
+        private void Setup_Load(object sender, EventArgs e)
+        { }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Setup.ActiveForm.Close();
+        }
+        private void btnExportToExcel_Click(object sender, EventArgs e)
+        {
+            Excel._Application app = new Excel.Application();
+            Excel._Workbook workbook = app.Workbooks.Add(Type.Missing);
+            Excel._Worksheet worksheet = null;
+            app.Visible = true;
+            worksheet = workbook.Sheets["Sheet1"];
+            worksheet = workbook.ActiveSheet;
+
+            // changing the name of active sheet
+            worksheet.Name = "Setups-QuickCode";
+
+            for (int i = 1; i < dataGridView1.Columns.Count + 1; i++)
+            {
+                worksheet.Cells[1, i] = dataGridView1.Columns[i - 1].HeaderText;
+            }
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    worksheet.Cells[i + 2, j + 1] = dataGridView1.Rows[i].Cells[j].Value.ToString();
+                }
+            }
+
+            workbook.SaveAs("c:\\output.xlsx", Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+
+        }
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            
+            if (txtSearch.Text == string.Empty)
+            {
+                lblStatus.Text = "Please enter a valid Description to Search";
+            }
+            else
+            {
+                int sortN = cmbSelect.SelectedIndex < 0 ? 1 : cmbSelect.SelectedIndex;
+                
+                String srchVale = Convert.ToString(txtSearch.Text);
+                BindingSource bs = new BindingSource();
+                bs.DataSource = dataGridView1.DataSource;
+                bs.Filter = dataGridView1.Columns[sortN].HeaderText.ToString() + " Like '%" + srchVale + "%'";
+                int rowcount = dataGridView1.RowCount;
+                lblStatus.Text = "Rows Returned :  " + (rowcount - 1).ToString();
+                dataGridView1.DataSource = bs;
+                btnClearSrch.Enabled = true;
+            }
+
+
+        }
+
+        private void btnClearSrch_Click(object sender, EventArgs e)
+        {
+            btnSearch.Enabled = false;
+            btnExportToExcel.Enabled = false;
+            txtSearch.Text = string.Empty;
+            dataGridView1.DataSource = null;
+            SeedTheGrid();
+        }
+    }
+
+}
+
